ef3eb55b2eb40dafcfebdbf458ba9fcd0c08474f
Yavor <d69@abv.bg>
2022-11-06 09:38:38 +0200

    change methods in Patient

---
+++ src/Inputs.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public abstract class Inputs {
+
+    static Scanner scan = new Scanner(System.in);
+
+    public static int inputPositiveInteger() {
+        while (!(scan.hasNextInt())) {
+            System.out.println("Invalid input. Try again!");
+            scan.next();
+        }
+        int num = scan.nextInt();
+        while (num < 1) {
+            System.out.println("Invalid input number. Try again!");
+            num = inputPositiveInteger();
+        }
+        return num;
+    }
+
+
+}
