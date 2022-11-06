1a24fc12a247b4b8180adf52e1254c024ea1c60e
Yavor <d69@abv.bg>
2022-11-06 10:25:57 +0200

    recovery

--- src/Hospital.java
+++ src/Hospital.java
@@ -11,6 +11,9 @@ public abstract class Hospital {
     String a = Db.specialities[1].name; // todo DELETE
 
 
+// --------------------------------------------------------------------------
+// ---------------------common-------------------------------------------------
+
     public static void showDoctors() {
         //  ---------------------------------------------------- TODO
     }
@@ -31,7 +34,9 @@ public abstract class Hospital {
         // ---------------------------------------------------- TODO
     }
 
-    //------ APPS ----
+    //---------------------------------------------------------------------------
+    // ------------- DOCTOR ---------- TODO
+
     public void readAppointments() {  //
         for (Doctor d : Db.doctors) {
             for (Appointment ap: Db.appointments) {
@@ -42,7 +47,8 @@ public abstract class Hospital {
     }
 
 
-    // ------------- DOCTOR ---------- TODO
+    //---------------------------------------------------------------------------
+// ---------------------BOSS-------------------------------------------------
 
     public void addDoctor() { }     // ------------- TODO
 
@@ -52,5 +58,59 @@ public abstract class Hospital {
 
     public void removeSpecialty() { }     // ------------- TODO
 
+
+    //---------------------------------------------------------------------------
+    // ------------- PATIENT ---------- TODO
+
+    public void showMyAppointments() {
+        for (Appointment app: Hospital.appointments) {
+            if (app.getPatientId()==this.patientId) {
+                System.out.println(app);
+            }
+        }
+    }
+    public void removeAppointment() {
+        System.out.println("Enter appointment Id to remove ...");
+        int idToRemove = Inputs.inputPositiveInteger();
+        int index = getMyAppointmentIndex(idToRemove);
+        if (index >= 0) {
+            Hospital.appointments.remove(index);
+        } else System.out.println("You do not have an appointment with such an ID");
+    }
+    private int getMyAppointmentIndex(int id) {
+        for (Appointment app: Hospital.appointments) {
+            if (app.getId()==id && app.getPatientId()==this.patientId) {
+                return Hospital.appointments.indexOf(app);
+            }
+        }
+        return -1;
+    }
+    public void changeAppointmentsDateTime() {
+        System.out.println("Enter appointment Id to change ...");
+        int idToChange = Inputs.inputPositiveInteger();
+        int index = getMyAppointmentIndex(idToChange);
+        if (index >= 0) {
+            int doctorId = Hospital.appointments.get(index).getDoctorId();
+            String date = chooseNewDate(doctorId);
+            String time = chooseNewTime(doctorId);
+            String time = chooseNewTime(doctorId, date);
+            Hospital.appointments.get(index).setDate(date);
+            Hospital.appointments.get(index).setTime(time);
+        } else System.out.println("You do not have an appointment with such an ID");
+    }
+
+    private String chooseNewDate(int doctorId) { return "dddd"; }      // ------------- TODO
+
+    private String chooseNewTime(int doctorId) { return "tttt"; }      // ------------- TODO
+    private String chooseNewTime(int doctorId, String date) { return "tttt"; }      // ------------- TODO
+
+    public void AddAppointment() { }     // ------------- TODO
+
+    public void showDocAppointments(int docId) { }     // ------------- TODO
+    public void showDocAppointments(int docId, String date) { }     // ------------- TODO
+
+    private boolean checkIfDateTimeIsFreeByDoc(String date, String time) { return false; }      // ------------- TODO
+
+    private boolean checkIfDateTimeIsFreeByMe(String date, String time) { return false; }      // ------------- TODO
 
 }
