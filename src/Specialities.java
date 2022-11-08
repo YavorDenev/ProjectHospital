4036a706471169a61181ffa8e8e8300d717d4f92
Yavor <d69@abv.bg>
2022-11-08 17:42:32 +0200

    methods in Hospital, Doctor and Specialities; DBase - comments

--- src/Specialities.java
+++ src/Specialities.java
@@ -8,4 +8,29 @@ public class Specialities {
     public String toString() {
         return "" + id + " " + name + " " + isHidden;
     }
+
+    public void showPatients() {
+        for (Patient p : DBase.patients) {
+            for (Appointment app: DBase.appointments) {
+
+                if (  app.patientID==p.id) {
+                    System.out.println(p);
+                    break;
+                }
+            }
+        }
+        /*
+        for (Patient p : DBase.patients) {
+            for (Doctor doc: DBase.doctors) {
+                for (Appointment app: DBase.appointments) {
+                    if (doc.speciality.equals(this.name) && app.doctorID==doc.id && app.patientID==p.id) {
+                    System.out.println(p);
+                    break;
+                    }
+                }
+            }
+        }
+         */
+    }
+
 }
