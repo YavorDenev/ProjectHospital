f4df853762c65dc54ba29a16e99c8d4733a4727b
Yavor <d69@abv.bg>
2022-11-20 13:45:19 +0200

    Unut tests for class Appointment

--- Test/AppointmentTest.java
+++ Test/AppointmentTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppointmentTest {
@@ -21,6 +24,36 @@ public class AppointmentTest {
         Appointment app = DBase.appointments.get(0);
         String expected = "Ivan, Alexiev";
         String actual = app.getPatientNames();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetPatientNamesWhenPatientIdIdWrong() {
+        ReadWrite.getPatientsFromFile("real_patients.txt");
+        Appointment app = new Appointment();
+        app.patientID = -1;
+        String expected = "";
+        String actual = app.getPatientNames();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void generateAppId() {
+        DBase.appointments = new ArrayList<>();
+        DBase.appointments.add(new Appointment());
+        DBase.appointments.get(0).id = 1000;
+        Appointment app = new Appointment(0,0,"x","x", 0);
+        int expected = 1001;
+        int actual = app.getId();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void generateAppIdWhenAppointmentsIsEmpty() {
+        DBase.appointments = new ArrayList<>();
+        Appointment app = new Appointment(0,0,"x","x", 0);
+        int expected = 1;
+        int actual = app.getId();
         assertEquals(expected, actual);
     }
 
