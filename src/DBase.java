b0933a057cc50d7e04b43a9605dff95f84fb7e84
Martin Katev <amaranthin@gmail.com>
2022-11-23 21:45:46 +0200

    change order of actions add new actions update actions.xls

--- src/DBase.java
+++ src/DBase.java
@@ -31,41 +31,42 @@ public abstract class DBase {
     };
 
     public static void initializeAllowedActions(){
-        allowedActions[0] = "logout"; //boss, doctor, patient  ----------> MARTIN - TODO
-        allowedActions[1] = "login as patient"; //anonymous  ------------> MARTIN - TODO
-        allowedActions[2] = "login as doctor"; //anonymous  -------------> MARTIN - TODO
-        allowedActions[3] = "login as boss"; //anonymous  ---------------> MARTIN - TODO
-        allowedActions[4] = "show list of doctors"; //boss, doctor, patient, anonymous ============> Hospital.showDoctors()
-        allowedActions[5] = "show list of patients"; //boss, doctor ===============================> Hospital.showPatients()
-        // липсва:  "show list of specialities"; //boss, doctor, patient, anonymous <---- НЕ СЕ ИСКА ПО УСЛОВИЕ =====> Hospital.showSpecialities();
-        allowedActions[6] = "show doctor calendar by doctor_id"; //boss, doctor  ==================> Doctor.showSortedDocApptsByCriteria(docId, Up/Down, SortCriteria.DATE_TIME);
-        allowedActions[7] = "show my calendar"; //doctor  =========================================> Doctor.showSortedDocApptsByCriteria(Up/Down, SortCriteria.DATE_TIME);
-        allowedActions[8] = "show my appointments"; //patient =====================================> patients.get(index).showMyAppointments()
-        allowedActions[9] = "show patient appointments by patient_id"; //boss, doctor =============> Patient.showAppointmentsByPatientId(patientId)
-        allowedActions[10] = "show sorted calendar on doctor_id by time"; //boss, doctor > Up/Down ============> Doctor.showSortedDocApptsByCriteria(docId, Up/Down, SortCriteria.DATE_TIME);
+
+        allowedActions[0] = "EXIT"; //boss, doctor, patient, anonymous  ----------> MARTIN - TODO
+        allowedActions[1] = "logout"; //boss, doctor, patient
+        allowedActions[2] = "login as patient"; //anonymous
+        allowedActions[3] = "login as doctor"; //anonymous
+        allowedActions[4] = "login as boss"; //anonymous
+        allowedActions[5] = "register as patient"; //anonymous //TODO
+        allowedActions[6] = "show list of specialities"; //boss, doctor, patient, anonymous <---- TODO =====> Hospital.showSpecialities();
+
+        allowedActions[7] = "show list of doctors"; //boss, doctor, patient, anonymous ============> Hospital.showDoctors()
+        allowedActions[8] = "show list of patients"; //boss, doctor ===============================> Hospital.showPatients()
+        allowedActions[9] = "show doctor calendar by doctor_id"; //boss, doctor, patient  ==================> Doctor.showSortedDocApptsByCriteria(docId, Up/Down, SortCriteria.DATE_TIME);
+        allowedActions[10] = "show my appointments"; //doctor, patient =====================================> Doctor.showSortedDocApptsByCriteria(...) OR patients.get(index).showMyAppointments()
+        allowedActions[11] = "show patient appointments by patient_id"; //boss, doctor =============> Patient.showAppointmentsByPatientId(patientId)
+        allowedActions[12] = "show sorted calendar on doctor_id by time"; //boss, doctor > Up/Down ============> Doctor.showSortedDocApptsByCriteria(docId, Up/Down, SortCriteria.DATE_TIME);
                                                                                                       // ======> Doctor.showSortedDocApptsByCriteria(Up/Down, SortCriteria.DATE_TIME);
-        allowedActions[11] = "show sorted calendar on doctor_id by patient_name"; //boss, doctor > Up/Down ====> Doctor.showSortedDocApptsByCriteria(docId, Up/Down, SortCriteria.PATIENT_NAMES);
+        allowedActions[13] = "show sorted calendar on doctor_id by patient_name"; //boss, doctor > Up/Down ====> Doctor.showSortedDocApptsByCriteria(docId, Up/Down, SortCriteria.PATIENT_NAMES);
                                                                                                         // ====> Doctor.showSortedDocApptsByCriteria(Up/Down, SortCriteria.PATIENT_NAMES);
-        allowedActions[12] = "show sorted calendar on doctor_id by patient_id"; //boss, doctor > Up/Down ======> Doctor.showSortedDocApptsByCriteria(docId, Up/Down, SortCriteria.PATIENT_ID);
+        allowedActions[14] = "show sorted calendar on doctor_id by patient_id"; //boss, doctor > Up/Down ======> Doctor.showSortedDocApptsByCriteria(docId, Up/Down, SortCriteria.PATIENT_ID);
                                                                                                       // ======> Doctor.showSortedDocApptsByCriteria(Up/Down, SortCriteria.PATIENT_ID);
-        allowedActions[13] = "show patients by doctor names"; //boss, doctor > Down
-        allowedActions[14] = "show patients by speciality"; //boss, doctor > Down
-        allowedActions[15] = "show patients by date"; //boss, doctor > Down
-        // "show patients by doctor names"; //boss, doctor <--- ИСКА СЕ ПО УСЛОВИЕ ====> Hospital.showPatientsByDocNames(firstName, lastName)
-        // "show patients by speciality"; //boss, doctor <----- ИСКА СЕ ПО УСЛОВИЕ ====> Hospital.showPatientsBySpeciality(speciality)
-        // "show patients by date"; //boss, doctor  <---------- ИСКА СЕ ПО УСЛОВИЕ ====> Hospital.showPatientsByDate(date)
-        allowedActions[16] = "change appointment by app_id"; //patient ================> patients.get(index).changeAppointmentsDateTime()
+        allowedActions[15] = "show patients by doctor names"; //boss, doctor > Down > Hospital.showPatientsByDocNames(firstName, lastName)
+        allowedActions[16] = "show patients by speciality"; //boss, doctor > Down Hospital.showPatientsBySpeciality(speciality)
+        allowedActions[17] = "show patients by date"; //boss, doctor > Down  Hospital.showPatientsByDate(date)
+
+        allowedActions[18] = "change appointment by app_id"; //patient ================> patients.get(index).changeAppointmentsDateTime()
         // липсва:  "add new appointment"; //patient   <---- НЕ СЕ ИСКА ПО УСЛОВИЕ ====> patients.get(index).AddAppointment(docId, typeOfExam, date, time)
-        allowedActions[17] = "reject appointment by app_id"; // patient ===============> patients.get(index).removeMyAppointment()
+        allowedActions[19] = "reject appointment by app_id"; // patient, doctor, boss ===============> patients.get(index).removeMyAppointment()
                              // doctor  <------- НЕ СЕ ИСКА ПО УСЛОВИЕ ================> doctors.get(index).removeDocAppointment()
                              // boss  <------- НЕ СЕ ИСКА ПО УСЛОВИЕ ==================> bosses.get(index).removeAppointment()
-        // ----- ЗАБЕЛЕЖКА: No 17 е реализиран с 3 различни метода, защото Пациент и Доктор могат да трият само свои часове, а Шефът - всички
-        allowedActions[18] = "change doctor visibility"; //boss  <---- НЕ СЕ ИСКА ПО УСЛОВИЕ =======> bosses.get(index).changeDoctorVisibility(int idToChangeVis)
-        allowedActions[19] = "add new doctor"; //boss  <------------------- НЕ СЕ ИСКА ПО УСЛОВИЕ =======> bosses.get(index).addDoctor(firstName, lastName, age, sex, speciality)
-        // липсва:  "add speciality"; //boss  <---------------------------- НЕ СЕ ИСКА ПО УСЛОВИЕ =======> bosses.get(index).addSpeciality(name)
-        // липсва:  "change speciality visibility"; //boss   <-------- НЕ СЕ ИСКА ПО УСЛОВИЕ =======> bosses.get(index).changeSpecVisibility(int idToChangeVis)
-        allowedActions[20] = "change users allowed actions"; //boss  -------> MARTIN - TODO
-        allowedActions[21] = "EXIT"; //boss, doctor, patient, anonymous  ----------> MARTIN - TODO
+        // ----- ЗАБЕЛЕЖКА: No 19 е реализиран с 3 различни метода, защото Пациент и Доктор могат да трият само свои часове, а Шефът - всички
+        allowedActions[20] = "add new doctor"; //boss  <------------------- НЕ СЕ ИСКА ПО УСЛОВИЕ =======> bosses.get(index).addDoctor(firstName, lastName, age, sex, speciality)
+        // MARTO> викам да пропуснем липсва:  "add speciality"; //boss  <---------------------------- НЕ СЕ ИСКА ПО УСЛОВИЕ =======> bosses.get(index).addSpeciality(name)
+        // МАРТО > викам да го пропуснем липсва:  "change speciality visibility"; //boss   <-------- НЕ СЕ ИСКА ПО УСЛОВИЕ =======> bosses.get(index).changeSpecVisibility(int idToChangeVis)
+        allowedActions[21] = "change users allowed actions"; //boss  -------> MARTIN - TODO
+
+
     }
 
     static class constSex{
