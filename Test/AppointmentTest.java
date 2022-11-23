dd568625eacf77f1ec3bf878ab379bfdcbc4d9e9
Yavor <d69@abv.bg>
2022-11-23 13:09:59 +0200

    unij tests

--- Test/AppointmentTest.java
+++ Test/AppointmentTest.java
@@ -44,6 +44,60 @@ public class AppointmentTest {
         assertEquals(expected, actual);
     }
 
+
+    @Test
+    public void testSortApptsByCriteriaPatientIdDown() {
+        Read.getAppointmentsFromFile("appointments.txt");
+
+        Appointment.sortApptsByCriteria(DBase.appointments, "DoWn", SortCriteria.PATIENT_ID);
+
+        boolean flag = true;
+        for (int i = 0; i < DBase.appointments.size()-2; i++) {
+            if (DBase.appointments.get(i).getPatientID() < DBase.appointments.get(i+1).getPatientID()) {
+                flag = false;
+                break;
+            }
+        }
+
+        assertTrue(flag);
+    }
+
+    @Test
+    public void testSortApptsByCriteriaDateTimeUp() {
+        Read.getAppointmentsFromFile("appointments.txt");
+
+        Appointment.sortApptsByCriteria(DBase.appointments, "UP", SortCriteria.DATE_TIME);
+
+        boolean flag = true;
+        for (int i = 0; i < DBase.appointments.size()-2; i++) {
+            if (DBase.appointments.get(i).getDateTimeComparingKey() > DBase.appointments.get(i+1).getDateTimeComparingKey()) {
+                flag = false;
+                break;
+            }
+        }
+
+        assertTrue(flag);
+    }
+
+    @Test
+    public void testSortApptsByCriteriaPatientNamesDown() {
+        Read.getAppointmentsFromFile("appointments.txt");
+
+        Appointment.sortApptsByCriteria(DBase.appointments, "Down", SortCriteria.PATIENT_NAMES);
+
+        boolean flag = true;
+        for (int i = 0; i < DBase.appointments.size()-2; i++) {
+            String names1 = DBase.appointments.get(i).getPatientNames();
+            String names2 = DBase.appointments.get(i+1).getPatientNames();
+            if (names1.compareTo(names2)<0) {
+                flag = false;
+                break;
+            }
+        }
+
+        assertTrue(flag);
+    }
+
     @Test
     public void testGenerateAppId() {
         DBase.appointments = new ArrayList<>();
@@ -62,23 +116,6 @@ public class AppointmentTest {
         int expected = 1;
         int actual = app.getId();
         assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testSortApptsByCriteriaPatientIdDown() {
-        Read.getAppointmentsFromFile("appointments.txt");
-
-        Appointment.sortApptsByCriteria(DBase.appointments, "DoWn", SortCriteria.PATIENT_ID);
-
-        boolean flag = true;
-        for (int i = 0; i < DBase.appointments.size()-2; i++) {
-            if (DBase.appointments.get(i).getPatientID() < DBase.appointments.get(i+1).getPatientID()) {
-                flag = false;
-                break;
-            }
-        }
-
-        assertTrue(flag);
     }
 
 }
