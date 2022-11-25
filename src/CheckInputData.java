93ff20db585e6a2b82ad62ef32ce89b01cd3e4c9
Yavor <d69@abv.bg>
2022-11-25 12:02:28 +0200

    improvements

--- src/CheckInputData.java
+++ src/CheckInputData.java
@@ -37,13 +37,13 @@ public abstract class CheckInputData {
         boolean isCorrect = true;
         for (int i=0; i<str.length();i++) {
             char c = str.charAt(i);
-            if (!Character.isAlphabetic(c)){
+            if (!Character.isAlphabetic(c) && c != '-'){
                 isCorrect = false;
                 break;
             }
         }
         if (!isCorrect) {
-            System.out.println("Please use only a-z, A-Z without spaces! Try again!");
+            System.out.println("Please use only a-z, A-Z and '-', without spaces! Try again!");
             str = inputAlphabeticalNonSpacesString();
         }
         return str;
