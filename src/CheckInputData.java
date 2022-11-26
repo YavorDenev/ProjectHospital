e861a508ffe71beb6a69f67f26b4afe961f0a349
Yavor <d69@abv.bg>
2022-11-26 14:08:13 +0200

    improvements

--- src/CheckInputData.java
+++ src/CheckInputData.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 
 public abstract class CheckInputData {
 
-    static Scanner scan = new Scanner(System.in);
-
     public static int inputPositiveInteger() {
-        while (!(scan.hasNextInt())) {
+        Scanner sc = new Scanner(System.in);
+        while (!(sc.hasNextInt())) {
             System.out.println("Invalid input. Try again!");
-            scan.next();
+            sc.nextLine();
         }
-        int num = scan.nextInt();
+        int num = sc.nextInt();
         while (num < 1) {
             System.out.println("Invalid input number. Try again!");
             num = inputPositiveInteger();
@@ -18,11 +17,12 @@ public abstract class CheckInputData {
     }
 
     public static int inputNotNegativeInteger() {
-        while (!(scan.hasNextInt())) {
+        Scanner sc = new Scanner(System.in);
+        while (!(sc.hasNextInt())) {
             System.out.println("Invalid input. Try again!");
-            scan.next();
+            sc.nextLine();
         }
-        int num = scan.nextInt();
+        int num = sc.nextInt();
         while (num < 0) {
             System.out.println("Invalid input number. Try again!");
             num = inputPositiveInteger();
--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
         //HARD LOGIN
         // DBase.currentUser =  Katev; // hard login
         // DBase.currentUser =  DBase.doctorsMap.get(2);  // hard login
-        //DBase.currentUser =  DBase.patients.get(1);
+        DBase.currentUser =  DBase.patients.get(1);
 
         //TestKatev.doTests();
 
